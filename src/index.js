65a38702ccf38686d56323b795de503027101b34
Igor Sprovieri <igorsprovieri@MacBook-Air-de-Igor.local>
2022-12-22 12:28:26 -0300

    Mongoose added and schema configured

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 const port = 3000;
+
+async function connectDatabase() {
+  await mongoose.connect("mongodb://localhost:27017");
+}
+
+const tasksListSchema = new mongoose.Schema({
+  name: String,
+  initialDate: { type: Date, default: Date.now },
+  time: Date,
+  description: String,
+  checked: Boolean,
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  connectDatabase().catch((error) => {
+    console.log(`Error to connect database`);
+  });
+  console.log(`App listening on port ${port}`);
 });
