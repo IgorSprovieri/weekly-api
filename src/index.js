54de4669d5d5ef90e95db9cbb4233cc94a4dfa10
Igor Sprovieri <igorsprovieri@MacBook-Air-de-Igor.local>
2022-12-22 14:05:41 -0300

    Nodemon added and post created

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 const app = express();
-const port = 3000;
+app.use(express.json());
+
+const port = 3333;
 
 async function connectDatabase() {
   await mongoose.connect("mongodb://localhost:27017");
@@ -9,10 +11,28 @@ async function connectDatabase() {
 
 const tasksListSchema = new mongoose.Schema({
   name: String,
-  initialDate: { type: Date, default: Date.now },
-  time: Date,
+  initialDate: Date,
+  finalDate: Date,
   description: String,
   checked: Boolean,
+});
+
+const listTask = mongoose.model("tasks", tasksListSchema);
+
+app.post("/task", async (req, res) => {
+  try {
+    const newTask = await listTask.create({
+      name: req.body.name,
+      initialDate: req.body.initialDate,
+      finalDate: req.body.finalDate,
+      description: req.body.description,
+      checked: req.body.checked,
+    });
+
+    return res.status(200).json(newTask);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
 });
 
 app.get("/", (req, res) => {
@@ -20,8 +40,9 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
+  mongoose.set("strictQuery", true);
   connectDatabase().catch((error) => {
-    console.log(`Error to connect database`);
+    console.log(error);
   });
   console.log(`App listening on port ${port}`);
 });
