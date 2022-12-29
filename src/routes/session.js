8839f448207e66b5728d583b40335001787e743a
Igor Sprovieri <igorsprovieri@MacBook-Air-de-Igor.local>
2022-12-29 12:13:26 -0300

    fix: delete router session

--- src/routes/session.js
+++ src/routes/session.js
@@ -40,18 +40,18 @@ router.get("/", async (req, res) => {
 
 router.delete("/logout/:id", async (req, res) => {
   const id = req.params.id;
-  const email = req.headers.email;
+  const user_id = req.headers.user_id;
 
   if (!id) {
     return res.status(400).json({ error: "Id is mandatory" });
   }
 
-  if (!email) {
+  if (!user_id) {
     return res.status(400).json({ error: "User email is mandatory" });
   }
 
   try {
-    const userFound = await usersList.find({ email: email });
+    const userFound = await usersList.findById(user_id);
 
     if (!userFound) {
       return res.status(404).json({ error: "User not found" });
@@ -63,12 +63,13 @@ router.delete("/logout/:id", async (req, res) => {
       return res.status(404).json({ error: "Session not found" });
     }
 
-    if (userFound._id != sessionFound.user_id) {
+    if (user_id != sessionFound.user_id) {
       return res.status(403).json({ error: "Acces denied" });
     }
   } catch (error) {
     return res.status(400).json(error);
   }
+
   try {
     await sessionsList.findByIdAndDelete(id);
 
