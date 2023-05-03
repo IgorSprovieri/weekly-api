4261e15b4719b685acf134b9a21e54c34c3297dd
Igor Sprovieri <igorsprovieri@MacBook-Air-de-Igor.local>
2023-05-03 11:37:41 -0300

    Fix: Cors

--- src/index.js
+++ src/index.js
@@ -8,12 +8,8 @@ const enviroment = process.env.ENVIROMENT;
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "*" }));
 app.use("/", Routes);
-app.use(
-  cors({
-    origin: "*",
-  })
-);
 
 const port = config[enviroment].port;
 
