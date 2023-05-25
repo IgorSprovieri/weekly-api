c96a5764bc86d76dcd6329dd8881293f1f0fdbbc
Igor Sprovieri <igsp1@hotmail.com>
2023-05-25 09:31:45 -0300

    Build: Cors updated

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const enviroment = process.env.ENVIROMENT;
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "*" }));
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 
 const port = config[enviroment].port;
 
