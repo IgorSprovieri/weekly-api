1b33392d5176ad277f740b8d1799f468cb48143b
Igor Sprovieri <igsp1@hotmail.com>
2023-06-12 15:10:42 -0300

    Config: Port

--- src/config/index.js
+++ src/config/index.js
@@ -1,15 +1,12 @@
 const config = {
   dev: {
     database: "mongodb://localhost:27017",
-    port: 3333,
   },
   test: {
     database: process.env.TEST_DB,
-    port: Number(process.env.TEST_PORT),
   },
   prod: {
     database: process.env.PROD_DB,
-    port: Number(process.env.PROD_PORT),
   },
 };
 
--- src/index.js
+++ src/index.js
@@ -15,16 +15,18 @@ app.use(cors(enviroment === "prod" ? corsOptions : "*"));
 app.use(express.json());
 app.use("/", Routes);
 
-const port = config[enviroment].port;
+const port = process.env.PORT || 3333;
 
 async function connectDatabase() {
   await mongoose.connect(config[enviroment].database);
 }
 
-app.listen(port, () => {
-  mongoose.set("strictQuery", true);
-  connectDatabase().catch((error) => {
+app.listen(port, "0.0.0.0", async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    await connectDatabase();
+    console.log(`App listening on port ${port}`);
+  } catch (error) {
     console.log(error);
-  });
-  console.log(`App listening on port ${port}`);
+  }
 });
