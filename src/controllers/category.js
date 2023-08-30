fd2d8811b9c439aaaa98304cd7a17ffada09758f
Igor Sprovieri <igsp1@hotmail.com>
2023-08-30 10:58:33 -0300

    Feat: Update category

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -38,6 +38,43 @@ class categoryController {
       return res.status(500).json({ error: error?.message });
     }
   }
+
+  async put(req, res) {
+    try {
+      const id = req.params.id;
+      const userId = req.userId;
+      const { name, hexColor } = req.body;
+
+      if (hexColor && !validateHexColor(hexColor)) {
+        return res.status(400).json({ error: "hexColor is invalid" });
+      }
+
+      const categoryFound = await categoriesList.findById(id);
+
+      if (!categoryFound) {
+        return res.status(404).json({ error: "Category not found" });
+      }
+
+      if (!categoryFound.user_id.equals(userId)) {
+        return res.status(401).json({ error: "Aceess denied" });
+      }
+
+      const updatedCategory = await categoriesList.findByIdAndUpdate(
+        id,
+        {
+          name: name,
+          hexColor: hexColor,
+        },
+        {
+          new: true,
+        }
+      );
+
+      return res.status(200).json(updatedCategory);
+    } catch (error) {
+      return res.status(500).json({ error: error?.message });
+    }
+  }
 }
 
 module.exports = new categoryController();
